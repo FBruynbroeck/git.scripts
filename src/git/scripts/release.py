7553d72b2516c2b1bbc4fef52c3467c0cf3dba88
FBruynbroeck <francois.bruynbroeck@hotmail.com>
2016-02-05 12:28:47 +0100

    Fix indent + remove line when 'Nothing changed yet'

--- src/git/scripts/release.py
+++ src/git/scripts/release.py
@@ -53,9 +53,12 @@ def updateBuildoutChangeLogs(history_lines, history_encoding, headings, changelo
     indentchangelogs = []
     for changelog in changelogs:
         if changelog:
-            changelog = '  %s' % changelog
+            changelog = '    %s' % changelog
         indentchangelogs.append(changelog)
     inject.extend(indentchangelogs)
+    inject.append('')
+    if history_lines[inject_location+3] == '- Nothing changed yet.':
+        del history_lines[inject_location+3:inject_location+6]
     history_lines[inject_location+2:inject_location+2] = inject
     contents = u'\n'.join(history_lines)
     utils.write_text_file(BUILDOUTHISTORYFILE, contents, history_encoding)
